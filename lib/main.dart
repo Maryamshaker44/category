2876f929d65d8bf7768edd4be7d5d94b8520493e
Maryamshaker44 <101062701+Maryamshaker44@users.noreply.github.com>
2025-01-11 16:40:21 +0200

    upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:category/feature/auth/model/data_auth/data_auth.dart';
 import 'package:category/my_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'core/bloc.dart';
 
 void main() {
   DataAuth dataAuth = DataAuth();
   dataAuth.PostData;
+  Bloc.observer = MyBlocObserver();
   runApp(const MyApp());
 }
